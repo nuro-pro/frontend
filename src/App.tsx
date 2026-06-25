e0e9ebd124834e9c697f28fc118d4f223509dcaa
03mminseok <iris15977@gmail.com>
2026-06-25 21:36:56 +0900

    [style] prettier 포맷팅 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Layout from './components/Layout'
 import LandingPage from './features/landing/LandingPage'
 
